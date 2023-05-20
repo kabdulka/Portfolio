7fc2b5239dedaadbc9457ddd14dbedaebdeabe3d
kabdulka <kenabdulka@gmail.com>
2023-05-20 13:43:53 -0700

    removed unused variable

--- src/components/Navbar/Navbar2.tsx
+++ src/components/Navbar/Navbar2.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import ReorderIcon from '@mui/icons-material/Reorder';
 import "../Navbar/Navbar2.scss"
