ba8baaa43dbe014f3041ff65a7d273e4edebb960
kabdulka <kenabdulka@gmail.com>
2023-05-20 16:29:46 -0700

    add dynamic projects

---
+++ src/Data/ProjectsInfo.ts
@@ -0,0 +1,57 @@
+
+import brainFlix from "../assets/images/brainflix.png"
+import inStock from "../assets/images/inStock.png"
+import movifi from "../assets/images/movifi.png"
+import auctions from "../assets/images/auctions.png"
+import encyclopedia from "../assets/images/encyclopedia.png"
+import connectFour from "../assets/images/connect4.png"
+
+interface projects {
+    name: string
+    image: string
+    skills: string
+    description: string
+}
+
+const projectsInfo: projects[] = [
+
+    {
+        name: "Instock",
+        image: inStock,
+        skills: "ReactJS, NodeJs, ExpressJS, Axios, MySQL, KnexJS, SASS, BEM, Git",
+        description: ""
+    },
+    {
+        name: "MoviFi",
+        image: movifi,
+        skills: "ReactJS, NodeJs, ExpressJS, Axios, MySQL, KnexJS, SASS, BEM, Git",
+        description: ""
+    },
+    {
+        name: "BrainFlix",
+        image: brainFlix,
+        skills: "ReactJS, NodeJs, ExpressJS, Axios, SASS, BEM, Git",
+        description: ""
+    },
+    {
+        name: "Auctions",
+        image: auctions,
+        skills: "Django, Python, SQLite, Git",
+        description: ""
+    },
+    {
+        name: "Connect Four",
+        image: connectFour,
+        skills: "Java",
+        description: ""
+    },
+    {
+        name: "Wiki Encyclopedia",
+        image: encyclopedia,
+        skills: "Django, Python, Git",
+        description: ""
+    },
+
+];
+
+export default projectsInfo
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,11 +1,12 @@
 
 import Project from "../../components/Project/Project";
 import "./Projects.scss";
-import brainFlix from "../../assets/images/brainflix.png";
-import inStock from "../../assets/images/inStock.png"
-import movifi from "../../assets/images/movifi.png"
-import auctions from "../../assets/images/auctions.png"
-import encyclopedia from "../../assets/images/encyclopedia.png"
+import myProjects from "../../Data/ProjectsInfo"
+// import brainFlix from "../../assets/images/brainflix.png";
+// import inStock from "../../assets/images/inStock.png"
+// import movifi from "../../assets/images/movifi.png"
+// import auctions from "../../assets/images/auctions.png"
+// import encyclopedia from "../../assets/images/encyclopedia.png"
 
 const Projects = () => {
 
@@ -13,11 +14,20 @@ const Projects = () => {
         <section className="projects">  
 
             <ul className="projects__list">
-                <Project image={brainFlix} name="BrainFlix"/>
+                {/* <Project image={brainFlix} name="BrainFlix"/>
                 <Project image={movifi} name="MoviFi"/>
                 <Project image={inStock} name="InStock"/>
                 <Project image={auctions} name="Auctions"/>
-                <Project image={encyclopedia} name="Wiki Encyclopedia"/>
+                <Project image={encyclopedia} name="Wiki Encyclopedia"/> */}
+                {
+                    myProjects.map( (project, index) => 
+                        <Project 
+                            key={index}
+                            image={project.image}
+                            name={project.name}
+                        />
+                    )
+                }
             </ul>
 
         </section>
