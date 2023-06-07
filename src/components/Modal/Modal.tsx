dcf8adeee206c9eb02ab866822b563ac06cb6a18
kabdulka <kenabdulka@gmail.com>
2023-06-06 23:46:23 -0700

    add style page

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,6 +1,7 @@
 
 import "./Modal.scss"
 import { Typewriter } from 'react-simple-typewriter'
+// import profilePicture from "../../assets/images/profileImage.jpg"
 
 interface ModalProps {
     handleModalChange: () => void
@@ -14,27 +15,44 @@ const Modal = ({handleModalChange}: ModalProps) => {
         <>
             <section className={`modal`}>
                 <h2 className="modal__header">
-                    modal open
+                    About me
                 </h2>
                 <div
-                className="main__about"
-                style={{ color: 'rgb(227, 134, 134)', fontWeight: 'bold', fontSize: '1.5rem' }}>
-          {/* Style will be inherited from the parent element */}
-        
-                <Typewriter
+                    className="main__about"
+                    style={{ color: 'rgb(227, 134, 134)', fontWeight: 'bold', fontSize: '1rem' }}>
+            
+                    <Typewriter
+                        
+                        words={['Full Stack Developer / Software Developer with a passion for learning and creating']}
+                        loop={1}
+                        cursor
+                        cursorStyle='>'
+                        typeSpeed={30}
+                        deleteSpeed={50}
+                        delaySpeed={1000}
                     
-                    words={['Full Stack Developer / Software Developer with a passion for learning and creating']}
-                    loop={2}
-                    cursor
-                    cursorStyle='>'
-                    typeSpeed={70}
-                    deleteSpeed={50}
-                    delaySpeed={1000}
-                
-                    // onLoopDone={handleDone}
-                    // onType={handleType}
-                />
-            </div>
+                        // onLoopDone={handleDone}
+                        // onType={handleType}
+                    />
+                </div>
+                <div
+                    className="main__about"
+                    style={{ color: 'rgb(227, 134, 134)', fontWeight: 'bold', fontSize: '1rem' }}>
+            
+                    <Typewriter
+                        
+                        words={['Full Stack Developer / Software Developer with a passion for learning and creating']}
+                        loop={1}
+                        cursor
+                        cursorStyle='>'
+                        typeSpeed={30}
+                        deleteSpeed={50}
+                        delaySpeed={1000}
+                    
+                        // onLoopDone={handleDone}
+                        // onType={handleType}
+                    />
+                </div>
 
             <div className="modal__close">
 
